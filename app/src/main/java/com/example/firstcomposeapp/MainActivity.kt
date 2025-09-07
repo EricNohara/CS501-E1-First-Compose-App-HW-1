00d9d82245d60beea015eaf25229f33c4ef54da0
EricNohara <ernohara@bu.edu>
2025-09-07 00:04:25 -0400

    Add changes to implement assignment requirements

--- app/src/main/java/com/example/firstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeapp/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firstcomposeapp.ui.theme.FirstComposeAppTheme
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +30,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             FirstComposeAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    DisplayMyInfo(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
                     )
                 }
             }
@@ -31,17 +42,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DisplayMyInfo(modifier: Modifier = Modifier) {
+    // two possible messages
+    val msg1 = "Message 1"
+    val msg2 = "Message 2"
+
+    // use state to hold current message
+    var curMsg by remember { mutableStateOf(msg1) }
+
+    // use column to make elements appear top to bottom
+    Column (
+        modifier = modifier.padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // text components
+        Text("Eric Nohara-LeClair")
+        Text("U90387562")
+        Text(curMsg)
+
+        // button logic
+        Button(onClick = {
+            curMsg = if (curMsg == msg1) msg2 else msg1
+        }) {
+            Text("Change Message")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     FirstComposeAppTheme {
-        Greeting("Android")
+        DisplayMyInfo()
     }
 }
